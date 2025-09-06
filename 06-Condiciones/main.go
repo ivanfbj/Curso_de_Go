24d8171cc52d18d4e3fe91ec1d6be4e680ce3cca
ivanfbj <ivanfbj@gmail.com>
2025-09-05 21:28:28 -0500

    Clase 6: Condicionales if y else en Go con operadores básicos

---
+++ 06-Condiciones/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	nombre := "Amin"
+	edad := 40
+
+	if nombre == "Amin" {
+		fmt.Println("Hola, " + nombre + "!")
+	} else {
+		fmt.Println("Hola desconocido!")
+	}
+
+	if edad >= 18 {
+		fmt.Println("Ya puedes votar")
+	}
+
+	if 9 % 2 == 0 {
+		fmt.Println("El 9 es par")
+	} else {
+		fmt.Println("El 9 es impar")
+	}
+
+	if numero := 99; numero < 0 {
+		fmt.Println(numero, "es negativo")
+	} else if numero < 10 {
+		fmt.Println(numero, "es de un solo dígito")
+	} else {
+		fmt.Println(numero, "es mayor a nueve")
+	}
+}
